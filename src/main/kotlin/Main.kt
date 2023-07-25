c7d0bbe9c17f28393da8005655b99c907363e109
Atul Anand <atulanandbhatt@gmail.com>
2023-07-25 15:24:11 +0530

    Revert "Basic Primary Constructor"
    This reverts commit bd4a47f475f5b7df19953b41661844ca8f4129db.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,3 @@
 fun main(args: Array<String>) {
-    var student=Student("Atul Anand")
-}
-
-class Student(var name:String)
-{
-    init {
-        println("My name is $name")
-    }
+    println("Hello World!")
 }
