78b647c4b84990c7e62697fe2b261d2eb44241df
Atul Anand <atulanandbhatt@gmail.com>
2023-07-25 08:46:15 +0530

    Infix Function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,27 @@
+
+/*
+*   INFIX FUNCTIONS
+* */
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val x: Int = 6
+    val y: Int = 10
+
+    val greaterVal = x findGreaterValue y   // x.findGreaterValue(y),
+                                            // advantage of infix keyword we can call -> x findGreaterValue y
+    println(greaterVal)
+
 }
+
+
+infix fun Int.findGreaterValue(other: Int): Int {  // INFIX and Extension Func because only one parameter
+
+    if (this > other)
+        return this
+    else
+        return other
+}
+/*
+*       1. All INFIX Functions are Extension functions
+*            But all Extension functions are not INFIX
+*       2. INFIX Functions just have ONE PARAMETER
+* */
