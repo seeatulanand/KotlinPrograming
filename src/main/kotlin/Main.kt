815296a191951bc2d9f1c3df5b09c3f5d9075906
Atul Anand <atulanandbhatt@gmail.com>
2023-07-25 15:37:49 +0530

    Basic Secondary Constructor

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,20 @@
 fun main(args: Array<String>) {
-    var student=Student("Atul Anand")
+
+    var student = Student("Steve", 10)
+
+    println(student.id)
 }
 
-class Student(var name:String)
-{
+class Student(var name: String) {
+
+    var id: Int = -1
+
     init {
-        println("Student name is $name")
+        println("Student has got a name as $name and id is $id")
+    }
+
+    constructor(n: String, id: Int): this(n) {
+        // The body of the secondary constructor is called after init block
+        this.id = id
     }
 }
