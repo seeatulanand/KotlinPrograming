23861de258fdc99ce2dfdc5152ab5924633f1bf1
Atul Anand <atulanandbhatt@gmail.com>
2023-07-28 22:29:44 +0530

    data class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,33 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+var student1=Student("Atul","1")
+var student2=Student("Atul","1")
+
+    /*println("Student 1 address -> $student1")
+    println("Student 2 address -> $student2")*/
+
+
+
+    if (student1==student2)
+    {
+     println("Equal")
+    }
+    else
+    {
+        println("Not Equal")
+    }
+
+    /*var student_copied= student1.copy(id="100",name = "Anand")
+    println(student_copied)*/
 }
+//dealing with data only.
+data class Student(var name:String,var id:String)
+{
+
+}
+
+
+//without data class it will give not equal
+/*class Student(var name:String,var id:String)
+{
+
+}*/
