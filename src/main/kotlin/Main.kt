dc5349b96aa355da7ad8b38af8cbc77a011282ac
Atul Anand <atulanandbhatt@gmail.com>
2023-07-30 11:49:14 +0530

    object declaration and companion object

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,56 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    //object class
+    /*CustomersData.count = 98
+    CustomersData.typeOfCustomers()
+
+    println(CustomersData.typeOfCustomers())
+
+    CustomersData.count = 109
+    println(CustomersData.count)
+
+    CustomersData.myMethod("hello")*/
+
+       println(MyClass.count)
+       println(MyClass.typeOfCustomers())
+
+
 }
+
+
+//companion object.
+
+class MyClass {
+   companion object  {  // Object Declaration
+
+        var count: Int = -1             // Behaves like a STATIC variable
+        //if we want to call this method from java class add @JvmStatic
+        @JvmStatic
+        fun typeOfCustomers(): String { // Behaves like a STATIC method
+            return "Indian"
+        }
+    }
+}
+
+
+
+/*
+open class MySuperClass {
+
+    open fun myMethod(str: String) {
+        println("MySuperClass")
+    }
+}
+
+object CustomersData: MySuperClass() {      // Object Declaration
+
+    var count: Int = -1             // Behaves like a STATIC variable
+
+    fun typeOfCustomers(): String { // Behaves like a STATIC method
+        return "Indian"
+    }
+
+    override fun myMethod(str: String) {    // Currently, behaving like a STATIC method
+        super.myMethod(str)
+        println("object Customer Data: $str")
+    }
+}*/
