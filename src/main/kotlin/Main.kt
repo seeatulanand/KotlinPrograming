2bb89b46e87066dbf29cef4e258a246bddd94ba5
Atul Anand <atulanandbhatt@gmail.com>
2023-07-31 17:17:48 +0530

    Array in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,25 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    var array = Array<Int>(5){0}
+    array[0]=1
+    array[1]=2
+    array[2]=3
+    array[3]=4
+    array[4]=5
+
+   /* for (i in 0..array.size-1)
+    {
+        //println(i)
+        println(array[i])
+
+    }*/
+
+
+    for (i in 0 until array.size)
+    {
+        //println(i)
+        println(array[i])
+
+    }
+
+
 }
