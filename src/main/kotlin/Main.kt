728b04f5791cf80f3571b970daf725a5609f9e2a
Atul Anand <atulanandbhatt@gmail.com>
2023-07-31 14:00:40 +0530

    it keyword in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,28 @@
+
+/*
+* 'it' keyword
+* */
 fun main(args: Array<String>) {
-    println("Hello World!")
+    var reverse = Reverse()
+    //reverse.reverseString("Atul"){ s -> s.reversed()}
+    reverse.reverseString("Atul"){ it.reversed()}
+
+}
+
+class Reverse
+{
+    //Normal way
+    /*fun reverseString(str:String,revstr:(String)->String)
+    {
+        var resultRev = revstr(str)
+        println(resultRev)
+
+    }*/
+    fun reverseString(str:String,revstr:(String)->String)  //// High Level Function with Lambda as Parameter
+   {
+       var resultRev = revstr(str) //// it.reversed() ==> str.reversed() ==> "Atul".reversed() = "lutA"
+       println(resultRev)
+
+   }
+
 }
