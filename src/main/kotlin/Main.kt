ba889c4e2c3aaf7e3378f1f9b9b4e0924fbc296a
Atul Anand <atulanandbhatt@gmail.com>
2023-07-31 16:50:23 +0530

    with and apply keyword

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,30 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+    var program=Program()
+
+    //with
+    with(program)
+    {
+        println(name)
+        println(id)
+    }
+    //apply
+    program.apply {
+        println(name)
+        println(id)
+
+    }.startProgram()
+
+
+}
+
+class Program
+{
+    var name="p1"
+    var id="-1"
+
+    fun startProgram()
+    {
+        println("start Program")
+    }
 }
