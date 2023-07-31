72ba475f11cfe30804b9407d6b079945b045b659
Atul Anand <atulanandbhatt@gmail.com>
2023-07-31 13:07:28 +0530

    kotlin closures

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,15 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    var program = MyProgram()
+    var result = 0;
+    program.addTwoNumber(7, 8) { a, b -> result = a + b }
+
+    println(result)
+
+}
+
+class MyProgram {
+    fun addTwoNumber(a: Int, b: Int, addTwoNum: (Int, Int) -> Unit) {
+        addTwoNum(a, b)
+
+    }
 }
