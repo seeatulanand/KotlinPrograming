a622292d427863f42f327d683f66dc58603e710f
Atul Anand <atulanandbhatt@gmail.com>
2023-08-01 17:13:28 +0530

    lateinit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,15 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+    lateinit var name: String
+    name="Atul"
+    println(name)
+
 }
+
+
+// lateinit used only with mutable data type [ var ]
+// lateinit used only with non-nullable data type
+// lateinit values must be initialised before you use it
+// lateinit can be used with var only.
+
+// If you try to access lateinit variable without initializing it then it throws UninitializedPropertyAccessException
