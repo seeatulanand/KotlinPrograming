dd8e61ba9c33f46ece84fb4a59395a3a587cf487
Atul Anand <atulanandbhatt@gmail.com>
2023-08-02 10:43:12 +0530

    Backing field

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,20 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    var human= Human()
+    println(human.age)
+
+}
+
+class Human {
+    var age = 20
+     get() {
+            //println("Age $age") // throw stackoverflow Exception.
+            println("Age $field")
+            return field
+        }
+        set(value) {
+            if (value >= 0) {
+                field = value
+            }
+
+        }
 }
